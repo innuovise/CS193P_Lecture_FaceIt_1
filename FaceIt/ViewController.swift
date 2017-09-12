32d30bd5d6505a4f732afda1e200d71296e9ac33
SK <sk@SKs-MacBook-Pro.local>
2017-09-12 23:34:34 +0900

    20170912_11:34PM

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by SK on 6/23/17.
+//  Copyright © 2017 SK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
